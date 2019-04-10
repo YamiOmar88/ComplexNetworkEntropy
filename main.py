7e28b01cc2550462810d7b449ebaf397cf9a0453
Yamila Omar <yamila.omar@uni.lu>
2019-04-10 13:28:54 +0200

    main code for using the classes defined

---
+++ main.py
@@ -0,0 +1,56 @@
+# Bosch Data Analysis: Entropy Centrality
+# Author: Yamila M. Omar
+# Date: 10/4/2019
+
+import GraphFile
+import Graph 
+
+# Some useful functions
+# =====================
+def normalize(edges):
+    '''This function allows to set edge weights in a 0 to 1 scale.
+    Input variable: edges (dictionary with edges (i,j) as keys and 
+    weights w_ij as values.
+    The function returns a new dictionary with normalized edge weights.'''
+    totSum = 0
+    for k,v in edges.items():
+        if k[0] == 'i':
+            totSum += v
+    normalized_edges = {}
+    for k,v in edges.items():
+        normalized_edges[k] = round(v/totSum, 5)
+    return normalized_edges
+    
+def remove_underutilized_edges(normalized_edges, tolerance=0.05):
+    ''' This function removes edges whose weight is below a tolerance.
+    Input variables: normalized_edges (dictionary) and tolerance (float).'''
+    new_dict = {}
+    for k,v in normalized_edges.items():
+        if v >= tolerance:
+            new_dict[k] = v
+    return new_dict
+
+
+# Read data
+# =========
+graph_to_study = "fullNetwork"
+file = GraphFile.GraphFile("data/" + graph_to_study + ".txt")
+edges = file.read_edges_from_file()
+
+# Process data
+# ============
+normalized_edges = normalize(edges)
+most_used_edges = remove_underutilized_edges(normalized_edges, tolerance=0.05)
+
+# Calculations / Results
+# =======================
+g = Graph.Graph(most_used_edges)
+C_H = g.entropyCentrality
+
+for k,v in C_H.items():
+    C_H[k] = abs(round(v, 5))
+    
+# Write to file
+# =============
+file = GraphFile.GraphFile("results/entropyCentrality_" + graph_to_study + ".txt")
+file.write_centrality_values_to_file(C_H)
