b26eb1eee52849f136b02b47d3ef224d13b728bb
Yamila Omar <yamila.omar@uni.lu>
2019-04-18 12:55:15 +0200

    entropy centrality calculations

--- main.py
+++ main.py
@@ -4,28 +4,46 @@
 
 import graphfile
 import graph 
-import lorenzcurve
+import dotfile
 
 # Read data
 # =========
-graph_to_study = input("Which graph should we study?\n Options: family1, family2, family3 and fullNetwork\n")
+while True:
+    graph_to_study = input("Which graph should we study?\nOptions: family1, family2, family3 and fullNetwork\n")
+    if graph_to_study in ["family1", "family2", "family3", "fullNetwork"]: 
+        break
+    else:
+        print("Enter a valid option!\n")
+
 file = graphfile.GraphFile("data/" + graph_to_study + ".txt")
 edges = file.read_edges_from_file()
 
 # Process data
 # ============
 g = graph.Graph(edges)
-most_used_edges = g.remove_underutilized_edges(tolerance=0.05)
+most_used_edges = g.remove_underutilized_edges(tolerance=0.001)
 new_g = graph.Graph(most_used_edges)
 
 # Calculations / Results
 # =======================
-# C_H = new_g.entropyCentrality
+C_H = new_g.entropyCentrality
 
-# for k,v in C_H.items():
-    # C_H[k] = abs(round(v, 5))
+for k,v in C_H.items():
+    C_H[k] = abs(v)
     
 # Write to file
 # =============
-# file = graphfile.GraphFile("results/entropyCentrality_" + graph_to_study + ".txt")
-# file.write_centrality_values_to_file(C_H)
+file = graphfile.GraphFile("results/entropyCentrality_" + graph_to_study + ".txt")
+file.write_centrality_values_to_file(C_H)
+
+
+# Read C_H from file
+# ==================
+C_H = graphfile.GraphFile("results/entropyCentrality_" + graph_to_study + ".txt").read_centrality_values_from_file()
+
+# Make dot file
+# =============
+filename = "entropyCentrality_" + graph_to_study + ".dot" 
+graph_names = {"family1": "F1", "family2": "F2", "family3": "F3", "fullNetwork": "FN"}
+dot = dotfile.DOT_File(filename, graph_names[graph_to_study], new_g, C_H)
+dot.save()
